60f44c2b8d36691d092e74f8432c6aca2aff693e
Miguel Hort <78941554+MiguelHort@users.noreply.github.com>
2023-10-23 22:53:08 -0300

    Add files via upload

---
+++ Download-js/script/back.js
@@ -0,0 +1,15 @@
+function coracao() {
+    const baixar = "/img/coracao.png";
+    const link = document.createElement('a');
+    link.href = baixar;
+    link.download = 'coracao.png'
+    link.click();
+}
+
+function circulo() {
+    const baixar = "/img/circulo.png";
+    const link = document.createElement('a');
+    link.href = baixar;
+    link.download = 'circulo.png'
+    link.click();
+}
